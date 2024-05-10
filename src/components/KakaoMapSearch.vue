47634597fd30699cd58911b8d741ae924f24dc8e
hennible0612 <sungwoo166@gmail.com>
2024-05-10 17:43:17 +0900

    검색수정

--- src/components/KakaoMapSearch.vue
+++ src/components/KakaoMapSearch.vue
@@ -73,10 +73,25 @@ function sendLocation(place) {
       background-color: rgba(255, 255, 255, 0.8);
       z-index: 5000;">
     <div class="option">
-      <form @submit.prevent="searchPlaces">
-        키워드: <input type="text" v-model="keyword" size="15" />
-        <button type="submit">검색하기</button>
+<!--      <form @submit.prevent="searchPlaces">-->
+<!--        키워드: <input type="text" v-model="keyword" size="15" />-->
+<!--        <button type="submit">검색하기</button>-->
+<!--      </form>-->
+
+      <form @submit.prevent="searchPlaces" class="search-form">
+        <input type="text" v-model="keyword" size="15" value="" placeholder="Search" class="search-input">
+        <button type="submit" class="search-button">
+          <svg class="submit-button">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search"></use>
+          </svg>
+        </button>
+
       </form>
+      <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" display="none">
+        <symbol id="search" viewBox="0 0 32 32">
+          <path d="M 19.5 3 C 14.26514 3 10 7.2651394 10 12.5 C 10 14.749977 10.810825 16.807458 12.125 18.4375 L 3.28125 27.28125 L 4.71875 28.71875 L 13.5625 19.875 C 15.192542 21.189175 17.250023 22 19.5 22 C 24.73486 22 29 17.73486 29 12.5 C 29 7.2651394 24.73486 3 19.5 3 z M 19.5 5 C 23.65398 5 27 8.3460198 27 12.5 C 27 16.65398 23.65398 20 19.5 20 C 15.34602 20 12 16.65398 12 12.5 C 12 8.3460198 15.34602 5 19.5 5 z" />
+        </symbol>
+      </svg>
     </div>
     <div id="menu_wrap" class="bg_white" style="
       overflow-y: auto;
@@ -89,11 +104,11 @@ function sendLocation(place) {
           @mouseout="handleMouseOut"
           @click="sendLocation(place)"
           class="leaderboard__profile">
-        <span style="color: red; font-size: 13px">{{ place.place_name }}</span>
-        <span style="color: orange; font-size: 13px">{{ place.road_address_name || place.address_name }}</span>
+        <b><span style="color: black; font-size: 13px">{{ place.place_name }}</span></b>
+        <span style="color: darkslategray; font-size: 13px">{{ place.road_address_name || place.address_name }}</span>
         <div>
-          <button style="color: yellow; font-size: 11px" @click="goToMap(place.place_url)">페이지</button>
-          <button style="color: yellow; font-size: 11px" @click="goToMap(place.place_url)">추가해</button>
+          <button style="color: darkslategray; font-size: 11px" @click="goToMap(place.place_url)">페이지</button>
+          <button style="color: deeppink; font-size: 11px" @click="goToMap(place.place_url)">추가해</button>
         </div>
 <!--        <button>리스트 추가하기</button>-->
       </li>
@@ -101,7 +116,9 @@ function sendLocation(place) {
   </div>
 </template>
 <style scoped>
+
 .map_wrap {
+  border-radius: 20px;
   position: relative; /* Ensure the scrollbar is only for the list content */
 }
 
@@ -234,5 +251,58 @@ body {
   font-size: 13px;
   margin-left: 3px;
 }
+.search-form {
+  position: relative;
+  top: 30px;
+  left: 50%;
+  width: 350px;
+  height: 40px;
+  border-radius: 40px;
+  box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);
+  transform: translate(-50%, -50%);
+  background: #fff;
+  transition: all 0.3s ease;
+}
 
+.search-form.focus {
+  box-shadow: 0 3px 4px rgba(0, 0, 0, 0.15);
+}
+
+.search-input {
+  position: absolute;
+  top: 10px;
+  left: 38px;
+  font-size: 14px;
+  background: none;
+  color: #5a6674;
+  width: 195px;
+  height: 20px;
+  border: none;
+  appearance: none;
+  outline: none;
+}
+
+.search-input::-webkit-search-cancel-button {
+  appearance: none;
+}
+
+.search-button {
+  position: absolute;
+  top: 10px;
+  left: 15px;
+  height: 20px;
+  width: 20px;
+  padding: 0;
+  margin: 0;
+  border: none;
+  background: none;
+  outline: none;
+  cursor: pointer;
+}
+
+.search-button svg {
+  width: 20px;
+  height: 20px;
+  fill: #5a6674;
+}
 </style>
