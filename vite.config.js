b9c3a2d1d35794291b11f5c0dbd0aeb685a76c03
hennible0612 <sungwoo166@gmail.com>
2024-05-23 10:47:21 +0900

    g

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0',  // 외부에서 접근 가능하도록 설정
+    port: 5173,        // 원하는 포트 번호로 설정
+    // 필요하다면 여기에 더 많은 서버 설정을 추가할 수 있습니다.
   }
 })
